a77aa35f5ade7f852ce86023814f60a3a90b7fd5
guttih <gudjonholm@gmail.com>
2017-05-13 23:50:23 +0000

    a little fix on the NodeMcu program

--- hardware/ArdosServerNodeMCU.ino
+++ hardware/ArdosServerNodeMCU.ino
@@ -887,7 +887,7 @@ String Gurl::removeLastSpaceIfExists(String str) {
 
 String Gurl::jsonKeyValue(String key, String value) {
     String   str = "\"" + key + "\":" + value;
-    return str;fgv
+    return str;
 }
 
 String Gurl::jsonObjectType(unsigned int uiType) {
